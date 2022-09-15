50151d4ffc77fea51973f3bf7724eb9003931e4a
ymdysk <ymdysk@sic.shimano.co.jp>
2022-09-15 10:19:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yusuke/kotlinlog/Human.kt
@@ -0,0 +1,24 @@
+package jp.techacademy.yusuke.kotlinlog
+
+import android.util.Log
+
+open class Human: Animal, Thinkable{
+
+    var hobby: String
+
+    // 引数付きコンストラクタ
+    constructor(name: String, age: Int, hobby: String): super(name, age) {
+        this.hobby = hobby
+    }
+
+    // Animalクラスのメソッドをオーバーライド
+    override fun say() {
+        Log.d("kotlintest", "私の名前は"+ this.name + "です。年は" + this.age + "歳です。")
+    }
+
+    // Thinkableインターフェイスのメソッドをオーバーライド
+    override fun think() {
+        Log.d("kotlintest", "私は" + hobby + "について考える。"+"（"+hobby+"はhobbyから取得）")
+    }
+
+}
